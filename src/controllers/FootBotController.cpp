c7464bd2605e59bd3e60812a3c1060eab558020e
WoutProvost <wout_provost@hotmail.com>
2020-05-16 05:10:52 +0200

    Consistency

--- src/controllers/FootBotController.cpp
+++ src/controllers/FootBotController.cpp
@@ -93,50 +93,6 @@ void FootBotController::Reset() {
 	coloredBlobOmnidirectionalCameraSensorEnabled = false;
 }
 
-CVector2 FootBotController::getRandomTurnDirectionVector() {
-	// Return a unit vector with a random angle	
-	return CVector2(1.0, ToRadians(CDegrees(random->Uniform(CRange<Real>(0.0, 360.0))))); // Interval is [min,max) i.e. right-open
-}
-
-bool FootBotController::adjustHeadingWithRandomTurnDirection(CVector2 &heading) {
-	// If the robot isn't currently performing obstacle avoidance, turn in a random direction every configured amount of timesteps to break up the grouping and mix the system better
-	bool ignoreNoTurn = false;
-	if(heading == CVector2::X) {
-		// Increase the amount of ticks since the robot last turned in a random direction
-		// Only increase the amount of ticks when not performing collision avoidance, so that over time the robots stop turning all at once
-		randomTurnTicks++;
-
-		// Check if the robot should turn in a random direction
-		if(randomTurnTicks == movementParams.getRandomTurnTicks()) {
-			// Get a unit vector that uses a random direction
-			randomTurnVector = getRandomTurnDirectionVector();
-
-			// Reset the ticks since the robot last turned in a random direction
-			randomTurnTicks = 0;
-		}
-
-		// If the random direction vector the robot last turned to isn't in its initial state
-		if(randomTurnVector != CVector2::ZERO) {
-			// Get readings from the positioning sensor
-			const CCI_PositioningSensor::SReading &reading = positioningSensor->GetReading();
-
-			// Determine the robot orientation
-			CRadians z; CRadians y; CRadians x;
-			reading.Orientation.ToEulerAngles(z, y, x);
-
-			// Change the heading to use the random direction
-			heading = CVector2(heading.Length(), randomTurnVector.Angle() - z);
-
-			// Make sure to use this new direction angle exactly
-			ignoreNoTurn = true;
-		}
-	} else {
-		// Reset the random direction vector the robot last turned to to its initial state so that the robot doesn't continually bump into the obstacle after completing the collision avoidance
-		randomTurnVector = CVector2::ZERO;
-	}
-	return ignoreNoTurn;
-}
-
 CColor FootBotController::getExitLightColorForRobotsToUse(const CColor &color) {
 	// Slightly alter the color so that it won't be detected as an exit
 	CColor exitLedsColor = color;
@@ -201,6 +157,50 @@ CVector2 FootBotController::getCollisionAvoidanceVector() {
 	else {
 		return -vectorToClosestObstacle.Normalize();
 	}
+}
+
+CVector2 FootBotController::getRandomTurnDirectionVector() {
+	// Return a unit vector with a random angle	
+	return CVector2(1.0, ToRadians(CDegrees(random->Uniform(CRange<Real>(0.0, 360.0))))); // Interval is [min,max) i.e. right-open
+}
+
+bool FootBotController::adjustHeadingWithRandomTurnDirection(CVector2 &heading) {
+	// If the robot isn't currently performing obstacle avoidance, turn in a random direction every configured amount of timesteps to break up the grouping and mix the system better
+	bool ignoreNoTurn = false;
+	if(heading == CVector2::X) {
+		// Increase the amount of ticks since the robot last turned in a random direction
+		// Only increase the amount of ticks when not performing collision avoidance, so that over time the robots stop turning all at once
+		randomTurnTicks++;
+
+		// Check if the robot should turn in a random direction
+		if(randomTurnTicks == movementParams.getRandomTurnTicks()) {
+			// Get a unit vector that uses a random direction
+			randomTurnVector = getRandomTurnDirectionVector();
+
+			// Reset the ticks since the robot last turned in a random direction
+			randomTurnTicks = 0;
+		}
+
+		// If the random direction vector the robot last turned to isn't in its initial state
+		if(randomTurnVector != CVector2::ZERO) {
+			// Get readings from the positioning sensor
+			const CCI_PositioningSensor::SReading &reading = positioningSensor->GetReading();
+
+			// Determine the robot orientation
+			CRadians z; CRadians y; CRadians x;
+			reading.Orientation.ToEulerAngles(z, y, x);
+
+			// Change the heading to use the random direction
+			heading = CVector2(heading.Length(), randomTurnVector.Angle() - z);
+
+			// Make sure to use this new direction angle exactly
+			ignoreNoTurn = true;
+		}
+	} else {
+		// Reset the random direction vector the robot last turned to to its initial state so that the robot doesn't continually bump into the obstacle after completing the collision avoidance
+		randomTurnVector = CVector2::ZERO;
+	}
+	return ignoreNoTurn;
 }
 
 void FootBotController::setWheelVelocitiesFromVector(const CVector2 &heading, bool ignoreNoTurn) {
