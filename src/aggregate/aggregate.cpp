e57c17c243ee0bd1c349dbdb40dfec310d70dde6
WoutProvost <wout_provost@hotmail.com>
2020-05-31 09:51:56 +0200

    Update aggregate script

--- src/aggregate/aggregate.cpp
+++ src/aggregate/aggregate.cpp
@@ -11,8 +11,8 @@ int main(int argc, char *argv[]) {
 	int finishedExperiments = 0;
 	int finalDecisions[3] = {0, 0, 0}; // Inconclusive, correct result, incorrect result
 	int finalResponses[3] = {0, 0, 0}; // Inconclusive, correct result, incorrect result
-	int earliestDecisions[3] = {0, 0, 0}; // Inconclusive, correct result, incorrect result
-	int earliestResponses[3] = {0, 0, 0}; // Inconclusive, correct result, incorrect result
+	int earliestDecisions = 0;
+	int earliestResponses = 0;
 
 	for(int arg = 1; arg < argc; arg++) {
 		ifstream input(argv[arg]);
@@ -44,7 +44,6 @@ int main(int argc, char *argv[]) {
 				if(earliestDecisionTime[2] == 0 && decisionData[2] >= THRESHOLD) {
 					earliestDecisionTime[2] = lineCount;
 				}
-				earliestDecisionTime[0] = 36000;
 
 				// Response (only use first occurence)
 				data >> c >> responseData[0] >> c >> responseData[1] >> c >> responseData[2];
@@ -54,7 +53,6 @@ int main(int argc, char *argv[]) {
 				if(earliestResponseTime[2] == 0 && responseData[2] >= THRESHOLD) {
 					earliestResponseTime[2] = lineCount;
 				}
-				earliestResponseTime[0] = 36000;
 
 				// Final result
 				if(prefix == "3.6e+06") {
@@ -63,25 +61,23 @@ int main(int argc, char *argv[]) {
 					// Decision
 					if(decisionData[1] >= THRESHOLD) {
 						finalDecisions[1]++;
-						earliestDecisions[1] += earliestDecisionTime[1];
+						earliestDecisions += earliestDecisionTime[1];
 					} else if(decisionData[2] >= THRESHOLD) {
 						finalDecisions[2]++;
-						earliestDecisions[2] += earliestDecisionTime[2];
+						earliestDecisions += earliestDecisionTime[2];
 					} else {
 						finalDecisions[0]++;
-						earliestDecisions[0] += earliestDecisionTime[0];
 					}
 
 					// Response
 					if(responseData[1] >= THRESHOLD) {
 						finalResponses[1]++;
-						earliestResponses[1] += earliestResponseTime[1];
+						earliestResponses += earliestResponseTime[1];
 					} else if(responseData[2] >= THRESHOLD) {
 						finalResponses[2]++;
-						earliestResponses[2] += earliestResponseTime[2];
+						earliestResponses += earliestResponseTime[2];
 					} else {
 						finalResponses[0]++;
-						earliestResponses[0] += earliestResponseTime[0];
 					}
 				}
 			}
@@ -91,21 +87,27 @@ int main(int argc, char *argv[]) {
 
 	double finalDecisionProbabilities[3] = {0.0, 0.0, 0.0};
 	double finalResponseProbabilities[3] = {0.0, 0.0, 0.0};
-	double earliestDecisionTimesteps[3] = {0.0, 0.0, 0.0};
-	double earliestResponseTimesteps[3] = {0.0, 0.0, 0.0};
 	for(int i = 0; i < 3; i++) {
 		finalDecisionProbabilities[i] = static_cast<double>(finalDecisions[i])/finishedExperiments*100;
 		finalResponseProbabilities[i] = static_cast<double>(finalResponses[i])/finishedExperiments*100;
-		earliestDecisionTimesteps[i] = static_cast<double>(earliestDecisions[i])/finalDecisions[i];
-		earliestResponseTimesteps[i] = static_cast<double>(earliestResponses[i])/finalResponses[i];
 	}
 
+	double earliestDecisionTimesteps = static_cast<double>(earliestDecisions)/(finalDecisions[1] + finalDecisions[2]);
+	double earliestResponseTimesteps = static_cast<double>(earliestResponses)/(finalResponses[1] + finalResponses[2]);
+
 	if(finishedExperiments == argc - 1) {
+		cout.setf(std::ios::fixed);
+		cout.precision(2);
+		
 		cout << finishedExperiments << " results aggregated (threshold=" << THRESHOLD*100 << "%): " << endl
-		<< "- Decision: T=" << finalDecisionProbabilities[1] << "% (" << earliestDecisionTimesteps[1] << "t)"
-		<< ", F=" << finalDecisionProbabilities[2] << "% (" << earliestDecisionTimesteps[2] << "t), I=" << finalDecisionProbabilities[0] << "% (" << earliestDecisionTimesteps[0] << "t)" << endl
-		<< "- Response: T=" << finalResponseProbabilities[1] << "% (" << earliestResponseTimesteps[1] << "t)"
-		<< ", F=" << finalResponseProbabilities[2] << "% (" << earliestResponseTimesteps[2] << "t), I=" << finalResponseProbabilities[0] << "% (" << earliestResponseTimesteps[0] << "t)" << endl;
+		<< "- Decision: T=" << finalDecisionProbabilities[1] << "%, F=" << finalDecisionProbabilities[2] << "%, I=" << finalDecisionProbabilities[0] << "%, t=" << earliestDecisionTimesteps << " timesteps" << endl
+		<< "- Response: T=" << finalResponseProbabilities[1] << "%, F=" << finalResponseProbabilities[2] << "%, I=" << finalResponseProbabilities[0] << "%, t=" << earliestResponseTimesteps << " timesteps" << endl;
+		
+		// cout << finalDecisionProbabilities[1] << "%\t" << finalDecisionProbabilities[2] << "%\t" << finalDecisionProbabilities[0] << "%" << endl;
+		// cout << finalResponseProbabilities[1] << "%\t" << finalResponseProbabilities[2] << "%\t" << finalResponseProbabilities[0] << "%" << endl;
+
+		// cout << earliestDecisionTimesteps << endl;
+		// cout << earliestResponseTimesteps << endl;
 	}
 	return 0;
 }
