e874cc138794d03ea0009c1f787f4e6fe4480edd
WoutProvost <wout_provost@hotmail.com>
2020-06-14 18:07:28 +0200

    Aggregate scripts with increasing difficulty

--- src/aggregate/aggregate-multiple.sh
+++ src/aggregate/aggregate-multiple.sh
@@ -16,6 +16,41 @@ if [ $? -eq 0 ]; then
 else
 	echo "No data"
 fi
+echo "----------NON-SYMMETRIC y=-5.5----------"
+ls individual_5.5_*.csv > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+	~/Desktop/aggregate-non-symmetric individual_5.5_*.csv
+else
+	echo "No data"
+fi
+echo "----------NON-SYMMETRIC y=-4.5----------"
+ls individual_4.5_*.csv > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+	~/Desktop/aggregate-non-symmetric individual_4.5_*.csv
+else
+	echo "No data"
+fi
+echo "----------NON-SYMMETRIC y=-3.5----------"
+ls individual_3.5_*.csv > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+	~/Desktop/aggregate-non-symmetric individual_3.5_*.csv
+else
+	echo "No data"
+fi
+echo "----------NON-SYMMETRIC y=-2.5----------"
+ls individual_2.5_*.csv > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+	~/Desktop/aggregate-non-symmetric individual_2.5_*.csv
+else
+	echo "No data"
+fi
+echo "----------NON-SYMMETRIC y=-1.5----------"
+ls individual_1.5_*.csv > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+	~/Desktop/aggregate-non-symmetric individual_1.5_*.csv
+else
+	echo "No data"
+fi
 echo ""
 
 echo "==========MAJORITY=========="
@@ -30,6 +65,41 @@ echo "----------NON-SYMMETRIC----------"
 ls majority_!(0.5)_*.csv > /dev/null 2>&1
 if [ $? -eq 0 ]; then
 	~/Desktop/aggregate-non-symmetric majority_!(0.5)_*.csv
+else
+	echo "No data"
+fi
+echo "----------NON-SYMMETRIC y=-5.5----------"
+ls majority_5.5_*.csv > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+	~/Desktop/aggregate-non-symmetric majority_5.5_*.csv
+else
+	echo "No data"
+fi
+echo "----------NON-SYMMETRIC y=-4.5----------"
+ls majority_4.5_*.csv > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+	~/Desktop/aggregate-non-symmetric majority_4.5_*.csv
+else
+	echo "No data"
+fi
+echo "----------NON-SYMMETRIC y=-3.5----------"
+ls majority_3.5_*.csv > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+	~/Desktop/aggregate-non-symmetric majority_3.5_*.csv
+else
+	echo "No data"
+fi
+echo "----------NON-SYMMETRIC y=-2.5----------"
+ls majority_2.5_*.csv > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+	~/Desktop/aggregate-non-symmetric majority_2.5_*.csv
+else
+	echo "No data"
+fi
+echo "----------NON-SYMMETRIC y=-1.5----------"
+ls majority_1.5_*.csv > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+	~/Desktop/aggregate-non-symmetric majority_1.5_*.csv
 else
 	echo "No data"
 fi
@@ -50,6 +120,41 @@ if [ $? -eq 0 ]; then
 else
 	echo "No data"
 fi
+echo "----------NON-SYMMETRIC y=-5.5----------"
+ls quality_5.5_*.csv > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+	~/Desktop/aggregate-non-symmetric quality_5.5_*.csv
+else
+	echo "No data"
+fi
+echo "----------NON-SYMMETRIC y=-4.5----------"
+ls quality_4.5_*.csv > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+	~/Desktop/aggregate-non-symmetric quality_4.5_*.csv
+else
+	echo "No data"
+fi
+echo "----------NON-SYMMETRIC y=-3.5----------"
+ls quality_3.5_*.csv > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+	~/Desktop/aggregate-non-symmetric quality_3.5_*.csv
+else
+	echo "No data"
+fi
+echo "----------NON-SYMMETRIC y=-2.5----------"
+ls quality_2.5_*.csv > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+	~/Desktop/aggregate-non-symmetric quality_2.5_*.csv
+else
+	echo "No data"
+fi
+echo "----------NON-SYMMETRIC y=-1.5----------"
+ls quality_1.5_*.csv > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+	~/Desktop/aggregate-non-symmetric quality_1.5_*.csv
+else
+	echo "No data"
+fi
 echo ""
 
 echo "==========RANDOM=========="
@@ -64,6 +169,41 @@ echo "----------NON-SYMMETRIC----------"
 ls random_!(0.5)_*.csv > /dev/null 2>&1
 if [ $? -eq 0 ]; then
 	~/Desktop/aggregate-non-symmetric random_!(0.5)_*.csv
+else
+	echo "No data"
+fi
+echo "----------NON-SYMMETRIC y=-5.5----------"
+ls random_5.5_*.csv > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+	~/Desktop/aggregate-non-symmetric random_5.5_*.csv
+else
+	echo "No data"
+fi
+echo "----------NON-SYMMETRIC y=-4.5----------"
+ls random_4.5_*.csv > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+	~/Desktop/aggregate-non-symmetric random_4.5_*.csv
+else
+	echo "No data"
+fi
+echo "----------NON-SYMMETRIC y=-3.5----------"
+ls random_3.5_*.csv > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+	~/Desktop/aggregate-non-symmetric random_3.5_*.csv
+else
+	echo "No data"
+fi
+echo "----------NON-SYMMETRIC y=-2.5----------"
+ls random_2.5_*.csv > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+	~/Desktop/aggregate-non-symmetric random_2.5_*.csv
+else
+	echo "No data"
+fi
+echo "----------NON-SYMMETRIC y=-1.5----------"
+ls random_1.5_*.csv > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+	~/Desktop/aggregate-non-symmetric random_1.5_*.csv
 else
 	echo "No data"
 fi
--- src/aggregate/aggregate-single.sh
+++ src/aggregate/aggregate-single.sh
@@ -16,6 +16,41 @@ if [ $? -eq 0 ]; then
 else
 	echo "No data"
 fi
+echo "----------NON-SYMMETRIC y=-2.5----------"
+ls individual_2.5_*.csv > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+	~/Desktop/aggregate-non-symmetric individual_2.5_*.csv
+else
+	echo "No data"
+fi
+echo "----------NON-SYMMETRIC y=-2.0----------"
+ls individual_2.0_*.csv > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+	~/Desktop/aggregate-non-symmetric individual_2.0_*.csv
+else
+	echo "No data"
+fi
+echo "----------NON-SYMMETRIC y=-1.5----------"
+ls individual_1.5_*.csv > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+	~/Desktop/aggregate-non-symmetric individual_1.5_*.csv
+else
+	echo "No data"
+fi
+echo "----------NON-SYMMETRIC y=-1.0----------"
+ls individual_1.0_*.csv > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+	~/Desktop/aggregate-non-symmetric individual_1.0_*.csv
+else
+	echo "No data"
+fi
+echo "----------NON-SYMMETRIC y=-0.5----------"
+ls individual_0.5_*.csv > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+	~/Desktop/aggregate-non-symmetric individual_0.5_*.csv
+else
+	echo "No data"
+fi
 echo ""
 
 echo "==========MAJORITY=========="
@@ -30,6 +65,41 @@ echo "----------NON-SYMMETRIC----------"
 ls majority_!(0.0)_*.csv > /dev/null 2>&1
 if [ $? -eq 0 ]; then
 	~/Desktop/aggregate-non-symmetric majority_!(0.0)_*.csv
+else
+	echo "No data"
+fi
+echo "----------NON-SYMMETRIC y=-2.5----------"
+ls majority_2.5_*.csv > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+	~/Desktop/aggregate-non-symmetric majority_2.5_*.csv
+else
+	echo "No data"
+fi
+echo "----------NON-SYMMETRIC y=-2.0----------"
+ls majority_2.0_*.csv > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+	~/Desktop/aggregate-non-symmetric majority_2.0_*.csv
+else
+	echo "No data"
+fi
+echo "----------NON-SYMMETRIC y=-1.5----------"
+ls majority_1.5_*.csv > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+	~/Desktop/aggregate-non-symmetric majority_1.5_*.csv
+else
+	echo "No data"
+fi
+echo "----------NON-SYMMETRIC y=-1.0----------"
+ls majority_1.0_*.csv > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+	~/Desktop/aggregate-non-symmetric majority_1.0_*.csv
+else
+	echo "No data"
+fi
+echo "----------NON-SYMMETRIC y=-0.5----------"
+ls majority_0.5_*.csv > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+	~/Desktop/aggregate-non-symmetric majority_0.5_*.csv
 else
 	echo "No data"
 fi
@@ -50,6 +120,41 @@ if [ $? -eq 0 ]; then
 else
 	echo "No data"
 fi
+echo "----------NON-SYMMETRIC y=-2.5----------"
+ls quality_2.5_*.csv > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+	~/Desktop/aggregate-non-symmetric quality_2.5_*.csv
+else
+	echo "No data"
+fi
+echo "----------NON-SYMMETRIC y=-2.0----------"
+ls quality_2.0_*.csv > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+	~/Desktop/aggregate-non-symmetric quality_2.0_*.csv
+else
+	echo "No data"
+fi
+echo "----------NON-SYMMETRIC y=-1.5----------"
+ls quality_1.5_*.csv > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+	~/Desktop/aggregate-non-symmetric quality_1.5_*.csv
+else
+	echo "No data"
+fi
+echo "----------NON-SYMMETRIC y=-1.0----------"
+ls quality_1.0_*.csv > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+	~/Desktop/aggregate-non-symmetric quality_1.0_*.csv
+else
+	echo "No data"
+fi
+echo "----------NON-SYMMETRIC y=-0.5----------"
+ls quality_0.5_*.csv > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+	~/Desktop/aggregate-non-symmetric quality_0.5_*.csv
+else
+	echo "No data"
+fi
 echo ""
 
 echo "==========RANDOM=========="
@@ -64,6 +169,41 @@ echo "----------NON-SYMMETRIC----------"
 ls random_!(0.0)_*.csv > /dev/null 2>&1
 if [ $? -eq 0 ]; then
 	~/Desktop/aggregate-non-symmetric random_!(0.0)_*.csv
+else
+	echo "No data"
+fi
+echo "----------NON-SYMMETRIC y=-2.5----------"
+ls random_2.5_*.csv > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+	~/Desktop/aggregate-non-symmetric random_2.5_*.csv
+else
+	echo "No data"
+fi
+echo "----------NON-SYMMETRIC y=-2.0----------"
+ls random_2.0_*.csv > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+	~/Desktop/aggregate-non-symmetric random_2.0_*.csv
+else
+	echo "No data"
+fi
+echo "----------NON-SYMMETRIC y=-1.5----------"
+ls random_1.5_*.csv > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+	~/Desktop/aggregate-non-symmetric random_1.5_*.csv
+else
+	echo "No data"
+fi
+echo "----------NON-SYMMETRIC y=-1.0----------"
+ls random_1.0_*.csv > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+	~/Desktop/aggregate-non-symmetric random_1.0_*.csv
+else
+	echo "No data"
+fi
+echo "----------NON-SYMMETRIC y=-0.5----------"
+ls random_0.5_*.csv > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+	~/Desktop/aggregate-non-symmetric random_0.5_*.csv
 else
 	echo "No data"
 fi
